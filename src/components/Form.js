3d78e4d42d4b112205a4d6b13c81fdf4c1613700
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-02-04 18:38:51 -0300

    style: estiliza formulário de cadastro de despesa

--- src/components/Form.js
+++ src/components/Form.js
@@ -37,30 +37,30 @@ class Form extends Component {
 
   render() {
     const { currencies, valuesToEdit, editting, editExpense } = this.props;
+    const inputClass = `px-2 border border-gray-600 focus:outline-none h-[23px]
+    focus:border-2 bg-transparent font-normal drop-shadow rounded focus:border-gray-600
+    ml-[5px]`;
+    const labelClass = 'mr-[10px]';
     return (
-      <form onChange={ this.getFormData }>
-        <label htmlFor="value-input">
-          Despesa:
+      <form
+        onChange={ this.getFormData }
+        className="drop-shadow-md text-gray-800
+        text-[17px] px-[20px] py-[15px] bg-white/95"
+      >
+        <h2 className="font-bold mb-[8px] text-[18px]">Registre a despesa</h2>
+        <label htmlFor="value-input" className={ labelClass }>
+          R$
           <input
             type="number"
             name="value"
             value={ valuesToEdit.value }
             data-testid="value-input"
-          />
-          R$
-        </label>
-        <label htmlFor="description-input">
-          Descrição:
-          <input
-            type="text"
-            name="description"
-            value={ valuesToEdit.description }
-            data-testid="description-input"
+            className={ `${inputClass} w-[100px]` }
           />
         </label>
-        <label htmlFor="currency">
-          Selecione a Moeda:
-          <select name="currency" id="currency">
+        <label htmlFor="currency" className={ labelClass }>
+          Moeda:
+          <select name="currency" id="currency" className={ `ml-[5px] ${inputClass}` }>
             {currencies.map((curr) => (
               <option key={ curr } value={ curr }>
                 {curr}
@@ -68,26 +68,28 @@ class Form extends Component {
             ))}
           </select>
         </label>
-        <label htmlFor="method-input">
-          Método de Pagamento:
+        <label htmlFor="method-input" className={ labelClass }>
+          Pagamento:
           <select
             name="method"
             id="method-input"
             value={ valuesToEdit.method }
             data-testid="method-input"
+            className={ `ml-[5px] ${inputClass}` }
           >
             <option value="Dinheiro">Dinheiro</option>
             <option value="Cartão de crédito">Cartão de crédito</option>
             <option value="Cartão de débito">Cartão de débito</option>
           </select>
         </label>
-        <label htmlFor="tag-input">
+        <label htmlFor="tag-input" className={ labelClass }>
           Tipo de Despesa:
           <select
             name="tag"
             id="tag-input"
             data-testid="tag-input"
             value={ valuesToEdit.tag }
+            className={ `ml-[5px] ${inputClass}` }
           >
             <option value="Alimentação">Alimentação</option>
             <option value="Lazer">Lazer</option>
@@ -96,14 +98,43 @@ class Form extends Component {
             <option value="Saúde">Saúde</option>
           </select>
         </label>
+        <label htmlFor="description-input" className={ labelClass }>
+          Descrição:
+          <input
+            type="text"
+            name="description"
+            value={ valuesToEdit.description }
+            data-testid="description-input"
+            className={ `ml-[5px] ${inputClass}` }
+          />
+        </label>
         {
           editting ? (
-            <button type="reset" onClick={ () => editExpense(valuesToEdit.id) }>
-              Editar despesa
+            <button
+              type="reset"
+              onClick={ () => editExpense(valuesToEdit.id) }
+            >
+              <span
+                className="material-symbols-outlined bg-gray-800 w-[23px] h-[23px]
+                rounded text-white translate-y-[6px] flex items-center justify-center
+                hover:bg-gray-600 duration-150"
+              >
+                edit
+              </span>
             </button>
           ) : (
-            <button type="reset" onClick={ this.setExpensesOnState }>
-              Adicionar despesa
+            <button
+              type="reset"
+              onClick={ this.setExpensesOnState }
+              className="w-[23px] h-[23px]"
+            >
+              <span
+                className="material-symbols-outlined bg-gray-800 w-[23px] h-[23px]
+                rounded text-white translate-y-[6px] flex items-center justify-center
+                hover:bg-gray-600 duration-150"
+              >
+                add
+              </span>
             </button>
           )
         }

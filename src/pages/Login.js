4cd539c6ac0fd5989cc345ede3a42143467085d9
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-02-04 18:39:15 -0300

    style: altera e conclui estilização na página de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { setEmail } from '../actions/index';
+import imgBank from '../assets/bank.jpg';
 
 class Login extends React.Component {
   state = {
@@ -38,42 +39,59 @@ class Login extends React.Component {
     const labelClass = 'my-[20px] font-bold';
 
     return (
-      <section
-        className="flex flex-col rounded drop-shadow-md items-center text-gray-900
-        text-[20px] p-[20px] drop-shadow bg-white/95"
-      >
-        <h2 className="text-center text-[30px] my-[10px]">Login</h2>
-        <label htmlFor="email-input" className={ labelClass }>
-          Email
-          <br />
-          <input
-            data-testid="email-input"
-            className={ inputClass }
-            type="email"
-            onChange={ this.validateLogin }
-          />
-        </label>
-        <label htmlFor="password-input" className={ labelClass }>
-          Senha
-          <br />
-          <input
-            data-testid="password-input"
-            className={ inputClass }
-            type="password"
-            minLength={ 6 }
-            onChange={ this.validateLogin }
-          />
-        </label>
-        <button
-          type="button"
-          className="bg-gray-900 rounded py-2 my-[10px] w-full
-          text-gray-100 disabled:bg-gray-700 disabled:text-gray-400 drop-shadow"
-          disabled={ !(validEmail && validPassword) }
-          onClick={ this.submitEmail }
+      <div className="flex bg-gray-400 h-[100vh] w-[100vw]">
+        <img
+          src={ imgBank }
+          alt="imagem de banco"
+          width="50%"
+          className="drop-shadow-[5px_0px_5px_rgba(0,0,0,0.25)] object-cover"
+        />
+        <section
+          className="flex flex-col rounded drop-shadow-md items-center text-gray-900
+          text-[17px] p-[20px] drop-shadow-xl bg-gray-200 h-max m-auto"
         >
-          Entrar
-        </button>
-      </section>
+          <a
+            href="https://icons8.com/icon/MSUWfu9MhE92/enter"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img
+              src="https://img.icons8.com/puffy/64/111827/experimental-enter-2-puffy.png"
+              alt="Ícone de login"
+            />
+          </a>
+          <label htmlFor="email-input" className={ labelClass }>
+            Email
+            <br />
+            <input
+              data-testid="email-input"
+              className={ inputClass }
+              type="email"
+              onChange={ this.validateLogin }
+            />
+          </label>
+          <label htmlFor="password-input" className={ labelClass }>
+            Senha
+            <br />
+            <input
+              data-testid="password-input"
+              className={ inputClass }
+              type="password"
+              minLength={ 6 }
+              onChange={ this.validateLogin }
+            />
+          </label>
+          <button
+            type="button"
+            className="bg-gray-900 rounded py-2 my-[10px] w-full
+            text-gray-100 disabled:bg-gray-700 disabled:text-gray-400 drop-shadow"
+            disabled={ !(validEmail && validPassword) }
+            onClick={ this.submitEmail }
+          >
+            Entrar
+          </button>
+        </section>
+      </div>
     );
   }
 }

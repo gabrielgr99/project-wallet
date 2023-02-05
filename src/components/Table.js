3f7bb6a631b455975862913e29200b1547cf2c12
Gabriel Gomes Rodrigues <gabrielbj99@gmail.com>
2023-02-05 14:00:29 -0300

    style: estiliza tabela de despesas

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Expense from './Expense';
+import './Table.css';
 
 class Table extends Component {
   componentDidUpdate() {
@@ -16,34 +17,36 @@ class Table extends Component {
       setValuesToEdit,
       idGenerator } = this.props;
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            expenses.map((expense) => (
-              <Expense
-                key={ expense.id }
-                expense={ expense }
-                getTotalValue={ getTotalValue }
-                setValuesToEdit={ setValuesToEdit }
-                idGenerator={ idGenerator }
-              />
-            ))
-          }
-        </tbody>
-      </table>
+      <div className="max-h-[500px] overflow-y-auto w-max mx-auto mt-[50px] shadow-lg">
+        <table>
+          <thead className="bg-gray-800 text-white rounded-t sticky top-0">
+            <tr>
+              <th className="py-[10px] w-[400px] rounded-tl-lg">Descrição</th>
+              <th className="w-[110px]">Tag</th>
+              <th className="w-[140px]">Pagamento</th>
+              <th className="w-[100px]">Valor</th>
+              <th className="w-[170px]">Moeda</th>
+              <th className="w-[90px]">Câmbio</th>
+              <th className="w-[120px]">Valor convertido</th>
+              <th className="w-[90px]">Moeda corrente</th>
+              <th className="rounded-tr-lg w-[130px]">Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              expenses.map((expense) => (
+                <Expense
+                  key={ expense.id }
+                  expense={ expense }
+                  getTotalValue={ getTotalValue }
+                  setValuesToEdit={ setValuesToEdit }
+                  idGenerator={ idGenerator }
+                />
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
